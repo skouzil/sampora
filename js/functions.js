547f1392506bb1db08810db76769bf8858c6ab9f
SevanDvd <sevan.david2a@gmail.com>
2024-10-24 11:36:24 +0200

    Check device2

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+
+// Fonction pour détecter si l'appareil est mobile ou non
+export function isMobileDevice() {
+    return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+}
+
+   
--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-window.onload = function() {
-    // Fonction pour détecter si l'appareil est mobile ou non
-    function isMobileDevice() {
-        return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
-    }
+import { isMobileDevice } from "js/functions.js";
 
-    // Exécuter la fonction lors du chargement de la page
-    if (isMobileDevice()) {
+window.onload = function() {
+
+     // Exécuter la fonction lors du chargement de la page
+     if (isMobileDevice()) {
         console.log("Vous utilisez un appareil mobile.");
         // Ici, vous pouvez ajouter d'autres actions spécifiques pour les appareils mobiles
     } else {
         console.log("Vous utilisez un ordinateur de bureau.");
         // Ici, vous pouvez ajouter d'autres actions spécifiques pour les ordinateurs
     }
-};
+}
+
